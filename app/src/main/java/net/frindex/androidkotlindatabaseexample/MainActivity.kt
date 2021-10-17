02b67d1ee6c4e7e5ee680b29e5bd9fbb57bfd14c
Sindre <sindredit@gmail.com>
2021-10-17 10:17:15 +0200

    xx

--- app/src/main/java/net/frindex/androidkotlindatabaseexample/MainActivity.kt
+++ app/src/main/java/net/frindex/androidkotlindatabaseexample/MainActivity.kt
@@ -1,11 +1,56 @@
 package net.frindex.androidkotlindatabaseexample
 
+import android.database.Cursor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.facebook.stetho.Stetho
+import com.facebook.stetho.okhttp3.StethoInterceptor
+import net.frindex.androidkotlindatabaseexample.dao.DBAdapter
+import okhttp3.OkHttpClient
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-}
+
+
+        /*- Stetho Database Tools */
+        // To browse database open Edge URL: edge://inspect/
+        Stetho.initializeWithDefaults(this);
+        OkHttpClient.Builder()
+            .addNetworkInterceptor(StethoInterceptor())
+            .build()
+
+
+        // Database
+        val db: DBAdapter = DBAdapter(this)
+
+        // Truncate (delete old data)
+        db.rawQuery("DELETE FROM games_index")
+
+        // Insert
+        db.rawQuery("INSERT INTO games_index (game_title, game_language) VALUES('Mitt spill', 'no')")
+        db.rawQuery("INSERT INTO games_index (game_title, game_language) VALUES('My game', 'en')")
+
+        // Select
+        var cursor: Cursor? = null
+        cursor = db.select("SELECT * FROM games_index ORDER BY game_id DESC")
+        val numberOfCols = cursor!!.columnCount
+        val numberOfRows = cursor.count
+        val rowsSaying = "Rows in table = " + numberOfRows
+        Toast.makeText(applicationContext,rowsSaying,Toast.LENGTH_SHORT).show()
+
+        while (cursor.moveToNext()) {
+            var id = cursor.getInt(0)
+            var title = cursor.getString(1)
+            var language = cursor.getString(2)
+
+            val output = "ID=" + id + " Title=" + title + " Language=" + language;
+            Toast.makeText(applicationContext, output, Toast.LENGTH_SHORT).show()
+        }
+
+
+    } // onCreate
+} // MainActivity
---
+++ app/src/main/java/net/frindex/androidkotlindatabaseexample/dao/DBAdapter.kt
@@ -0,0 +1,127 @@
+package net.frindex.androidkotlindatabaseexample.dao
+
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteException
+import android.database.sqlite.SQLiteOpenHelper
+
+class DBAdapter (context: Context): SQLiteOpenHelper(context,DATABASE_NAME,null,DATABASE_VERSION) {
+
+    /* 01 Object -------------------------------------------------------------------------- */
+    companion object {
+        private val DATABASE_VERSION = 1
+        private val DATABASE_NAME = "myDatabase"
+
+
+        // private val sQLiteOpenHelper: SQLiteOpenHelper? = null // Java: private DatabaseHelper DBHelper;
+        // private val db: SQLiteDatabase? = null // Java: private SQLiteDatabase db;
+    }
+
+
+    /*- 02 On Create ----------------------------------------------------------------------- */
+    override fun onCreate(db: SQLiteDatabase?) {
+        // Create tables
+        val createTableGamesIndex = ("CREATE TABLE games_index(" +
+                "game_id INTEGER PRIMARY KEY," +
+                "game_title TEXT, " +
+                "game_language TEXT" + ")")
+        db?.execSQL(createTableGamesIndex)
+
+    } // onCreate
+
+    /*- 03 On Upgrade ---------------------------------------------------------------------- */
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        // Drop tables
+        db!!.execSQL("DROP TABLE IF EXISTS games_index")
+
+        // Create tables
+        onCreate(db)
+    }
+
+    /*- 04 Open --------------------------------------------------------------------------- */
+    /*
+    fun open(){
+        // Java: db = DBHelper.getWritableDatabase();
+        db = this.writableDatabase
+    }*/
+
+    /*- 05 Close --------------------------------------------------------------------------- */
+    /*
+    fun close(){
+        // Java: DBHelper.close();
+        sQLiteOpenHelper.close()
+    }*/
+
+    /* 06 Quote smart ---------------------------------------------------------------------- */
+    fun quoteSmart(value: String?): String? {
+        // Escapes special characters in a string for use in an SQL statement
+        var value = value
+        if (value != null && value.length > 0) {
+            value = value.replace("\\", "\\\\")
+            value = value.replace("'", "\\'")
+            value = value.replace("\u0000", "\\0")
+            value = value.replace("\n", "\\n")
+            value = value.replace("\r", "\\r")
+            value = value.replace("\"", "\\\"")
+            value = value.replace("\\x1a", "\\Z")
+            value = value.replace("'", "&#39;")
+        }
+        value = "'$value'"
+
+        // Remove 'null'
+        value = value.replace("'null'", "NULL")
+        return value
+    }
+    fun quoteSmart(value: Double): Double {
+        return value
+    }
+    fun quoteSmart(value: Int): Int {
+        return value
+    }
+    fun quoteSmart(value: Long): Long {
+        return value
+    }
+
+
+    /*- 07 RawQuery ----------------------------------------------------------------- */
+    // Used for insert, update, delete, truncate
+    fun rawQuery(query: String?): String{
+
+        val dbAdapter = this.writableDatabase
+        dbAdapter.rawQuery(query, null)
+        dbAdapter.close()
+
+        return "ok";
+
+    }
+
+    /*- 08 Count -------------------------------------------------------------------- */
+    fun count(query: String?): Int?{
+        return try {
+            val db = this.writableDatabase
+            val mCount: Cursor = db.rawQuery(query + "", null)
+            mCount.moveToFirst()
+            val count = mCount.getInt(0)
+            mCount.close()
+            db.close()
+
+            count
+        } catch (e: SQLiteException) {
+            -1
+        }
+    }
+
+    /*- 09 Select ------------------------------------------------------------------ */
+    fun select(query: String?): Cursor? {
+        val dbAdapter = this.writableDatabase
+        val mCursor: Cursor = dbAdapter.rawQuery(query, null)
+        if (mCursor != null) {
+            mCursor.moveToFirst()
+        }
+        return mCursor
+        dbAdapter.close()
+    }
+
+
+} // class DBAdapter
